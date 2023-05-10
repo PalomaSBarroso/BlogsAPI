0e63577bd731b1d7507ccfbc12de04136d5a6172
Paloma Barroso <pbarroso15@gmail.com>
2023-05-10 18:10:28 -0300

    Requisito 1

---
+++ src/models/User.js
@@ -0,0 +1,30 @@
+
+module.exports = (sequelize, DataTypes) => {
+    const User = sequelize.define('User', {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true
+      },
+  
+      displayName: DataTypes.STRING,
+  
+      email: DataTypes.STRING,
+  
+      password: DataTypes.STRING,
+  
+      image: DataTypes.DATE,
+    }, {
+      timestamps: false,
+      underscored: true, 
+      tableName: 'users'
+    });
+  
+    User.associate = ({ BlogPost }) => {
+      User.hasMany(BlogPost, {
+        as: 'BlogPosts',
+        foreignKey: 'userId'
+      });
+    };
+  
+    return User;
+  };
